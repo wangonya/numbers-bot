2b321707593b60b50620a68b3cce78742de1b95d
= <kwangonya@gmail.com>
2019-10-05 21:55:45 +0300

    add custom action for numbers api

--- actions.py
+++ actions.py
@@ -1,27 +1,29 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
+import requests
 
-# This is a simple example for a custom action which utters "Hello World!"
+from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message("Hello World!")
-#
-#         return []
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class GetNumberFactAction(Action):
+
+    def name(self) -> Text:
+        return "get_number_fact"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        number = tracker.get_slot('number')
+        fact = requests.get(f'http://numbersapi.com/{number}')
+        if fact.status_code != 200:
+            dispatcher.utter_message("I'm sorry, I couldn't understand that 😞")
+        else:
+            dispatcher.utter_message(fact.text)
+
+        return []
+
